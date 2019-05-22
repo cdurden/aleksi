bd432ff20134d4581e550db6f66c892d9ea9e685
Chris Durden <cdurden@gmail.com>
2019-05-22 17:55:26 -0400

    adding multiple tuples to translations database for each lemma

--- aleksi/models/sanakirja.py
+++ aleksi/models/sanakirja.py
@@ -242,7 +242,7 @@ class Lemma(object):
             return(None)
 
     def to_dict(self):
-        return {'lemma': self.word, 'lang': self.lang, 'translations': [translation.to_dict for translation in self.translations]}
+        return {'lemma': self.word, 'lang': self.lang, 'translations': [translation.to_dict() for translation in self.translations]}
         #return(self.translation.to_dict())
 
 class WordMorph(object):
@@ -490,10 +490,10 @@ class Sanakirja(object):
             #remote=False
             for baseword in self.get_base_matches(word, lang):
                 translations = self.fetch_translations(baseword, lang, remote, fail_on_remote_call)
-                data['morpheme_translations'].append([translation.to_dict for translation in translations])
+                data['morpheme_translations'].append([translation.to_dict() for translation in translations])
         if not data['morpheme_translations']:
             translations = self.fetch_translations(word, lang, remote, fail_on_remote_call)
-            data['morpheme_translations'].append([translation.to_dict for translation in translations])
+            data['morpheme_translations'].append([translation.to_dict() for translation in translations])
         return(data)
     
 

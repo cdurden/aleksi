927d84adbf3257fbbb960ab6a15afb601b493a8e
Chris Durden <cdurden@gmail.com>
2019-05-24 14:10:46 -0400

    modifying plugin to work with firefox

--- aleksi_plugin/js/setup.js
+++
@@ -1,8 +0,0 @@
-var $jquery_aleksi = jQuery.noConflict();
-var mode = 'plugin';
-var settings;
-/*
-for (var setting in settings) {
-    settings[setting] = chrome.storage.sync.get({setting: settings[setting]}, function(result) { return; });
-}
-*/

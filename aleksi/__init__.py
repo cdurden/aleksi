0b232f8e08f6d3ecc024f7c5faa5e9173b3a34e8
Chris Durden <cdurden@gmail.com>
2019-07-21 00:51:19 -0400

    adding authentication policies

--- aleksi/__init__.py
+++ aleksi/__init__.py
@@ -304,7 +304,7 @@ def main(global_config, **settings):
     init_social(config, Base, DBSession)
     config.scan()
     config.scan('social_pyramid')
-    #config.set_authentication_policy(authn_policy)
+    config.set_authentication_policy(authn_policy)
     #config.set_authorization_policy(authz_policy)
 
 

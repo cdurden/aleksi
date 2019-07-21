56556a10084c6e925e7023c57ed94c06ae9b5483
Chris Durden <cdurden@gmail.com>
2019-07-21 05:24:18 -0400

    adding authentication policies

--- aleksi/social_auth_local_settings.py
+++ aleksi/social_auth_local_settings.py
@@ -1,6 +1,7 @@
 SOCIAL_AUTH_KEYS = {
     #'SOCIAL_AUTH_GOOGLE_OAUTH2_KEY': '688589481195-6a03sh0gvnrm41rjqglk3v623ruqsj2e.apps.googleusercontent.com',
-    'SOCIAL_AUTH_GOOGLE_OAUTH2_KEY': '688589481195-415as39tpoafhgs1fkb1clj6ve6f9q72.apps.googleusercontent.com',
+    #'SOCIAL_AUTH_GOOGLE_OAUTH2_KEY': '688589481195-415as39tpoafhgs1fkb1clj6ve6f9q72.apps.googleusercontent.com',
+    'SOCIAL_AUTH_GOOGLE_OAUTH2_KEY': '734929326024-et1isfam0pt8sb3v4atu7145v19phqde.apps.googleusercontent.com'
     'SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET': 'wXYXlx-BRs_oHDoiaE6WYFyw',
     'SOCIAL_AUTH_GOOGLE_PLUS_KEY': '688589481195-415as39tpoafhgs1fkb1clj6ve6f9q72.apps.googleusercontent.com',
     #688589481195-6a03sh0gvnrm41rjqglk3v623ruqsj2e.apps.googleusercontent.com',

39342c8131e7ac42499f011ca75005e1493f2404
Chris Durden <cdurden@gmail.com>
2019-07-21 03:55:54 -0400

    adding authentication policies

--- plugin/js/background.js
+++ plugin/js/background.js
@@ -342,7 +342,7 @@ function updateIconState(response) {
 function login(callback) {
     authUrl = settings.authUrl+"?next="+chrome.identity.getRedirectURL();
     chrome.identity.launchWebAuthFlow({'url': authUrl, 'interactive': true}, function (redirectUrl) {
-        chrome.cookies.get({"url": redirectUrl, "name": "session_key"}, function(cookie) {
+        chrome.cookies.get({"url": authUrl, "name": "session_key"}, function(cookie) {
                 a = cookie.value;
         });
         if (redirectUrl) {
